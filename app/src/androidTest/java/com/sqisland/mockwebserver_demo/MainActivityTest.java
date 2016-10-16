2757b9cf3c7e678bb5b31c7b4ba5635d8f18d65c
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-10-16 13:20:04 -0600

    Add MockWebServer

--- app/src/androidTest/java/com/sqisland/mockwebserver_demo/MainActivityTest.java
+++ app/src/androidTest/java/com/sqisland/mockwebserver_demo/MainActivityTest.java
@@ -1,11 +1,15 @@
 package com.sqisland.mockwebserver_demo;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.IOException;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -14,14 +18,28 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class MainActivityTest {
   @Rule
-  public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+  public ActivityTestRule<MainActivity> activityRule
+      = new ActivityTestRule<>(MainActivity.class, true, false);
 
   @Rule
   public OkHttpIdlingResourceRule okHttpIdlingResourceRule = new OkHttpIdlingResourceRule();
 
   @Test
   public void followers() throws IOException {
+    MockWebServer server = new MockWebServer();
+    server.start();
+
+    TestDemoApplication app = (TestDemoApplication)
+        InstrumentationRegistry.getTargetContext().getApplicationContext();
+    app.setBaseUrl(server.url("/").toString());
+
+    server.enqueue(new MockResponse().setBody("{ \"login\" : \"octocat\", \"followers\" : 1500 }"));
+
+    activityRule.launchActivity(null);
+
     onView(withId(R.id.followers))
-        .check(matches(withText("octocat: 1575")));
+        .check(matches(withText("octocat: 1500")));
+
+    server.shutdown();
   }
 }
--- app/src/androidTest/java/com/sqisland/mockwebserver_demo/TestDemoApplication.java
+++ app/src/androidTest/java/com/sqisland/mockwebserver_demo/TestDemoApplication.java
@@ -1,4 +1,14 @@
 package com.sqisland.mockwebserver_demo;
 
 public class TestDemoApplication extends DemoApplication {
+  private String baseUrl;
+
+  @Override
+  public String getBaseUrl() {
+    return baseUrl;
+  }
+
+  public void setBaseUrl(String url) {
+    baseUrl = url;
+  }
 }
