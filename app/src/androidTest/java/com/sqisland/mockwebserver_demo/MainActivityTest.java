db5aa5cd69d2b52729e264d70b2ba0b46ab4687c
Chiu-Ki Chan <chiuki@users.noreply.github.com>
2016-10-16 12:59:13 -0600

    Add MainActivityTest
    May fail if:
    1) The test looks at the TextView before the Retrofit call completes.
    2) The number of followers for octocat changes.

---
+++ app/src/androidTest/java/com/sqisland/mockwebserver_demo/MainActivityTest.java
@@ -0,0 +1,24 @@
+package com.sqisland.mockwebserver_demo;
+
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class);
+
+  @Test
+  public void followers() throws IOException {
+    onView(withId(R.id.followers))
+        .check(matches(withText("octocat: 1575")));
+  }
+}
